adbab64511834f006372ab6cdf220db0fe2c4b10
Sadeep Mihiranga <sandeep@hatchyard.io>
2023-02-20 14:23:48 +0530

    Initial commit

---
+++ src/main/java/lk/hatchyard/apigateway/ServiceFallBackController.java
@@ -0,0 +1,25 @@
+package lk.hatchyard.apigateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fall-back")
+public class ServiceFallBackController {
+
+    @GetMapping("/customer-service")
+    public String customerServiceFallBackMethod() {
+        return "Customer Service Not Working";
+    }
+
+    @GetMapping("/product-service")
+    public String productServiceFallBackMethod() {
+        return "Product Service Not Working";
+    }
+
+    @GetMapping("/order-service")
+    public String orderServiceFallBackMethod() {
+        return "Order Service Not Working";
+    }
+}
