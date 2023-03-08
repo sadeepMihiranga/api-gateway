12fb015904a0d13e53425066988c59d65c69eb56
Sadeep Mihiranga <sandeep@hatchyard.io>
2023-03-08 11:34:28 +0530

    FallBackMethod bug fixes

--- src/main/java/lk/hatchyard/apigateway/ServiceFallBackController.java
+++ src/main/java/lk/hatchyard/apigateway/ServiceFallBackController.java
@@ -1,6 +1,7 @@
 package lk.hatchyard.apigateway;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,17 +10,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class ServiceFallBackController {
 
     @GetMapping("/customer-service")
-    public String customerServiceFallBackMethod() {
+    public String customerServiceFallBackMethodGet() {
+        return "Customer Service Not Working";
+    }
+
+    @PostMapping("/customer-service")
+    public String customerServiceFallBackMethodPost() {
         return "Customer Service Not Working";
     }
 
     @GetMapping("/product-service")
-    public String productServiceFallBackMethod() {
+    public String productServiceFallBackMethodGet() {
+        return "Product Service Not Working";
+    }
+
+    @PostMapping ("/product-service")
+    public String productServiceFallBackMethodPost() {
         return "Product Service Not Working";
     }
 
     @GetMapping("/order-service")
-    public String orderServiceFallBackMethod() {
+    public String orderServiceFallBackMethodGet() {
+        return "Order Service Not Working";
+    }
+
+    @PostMapping("/order-service")
+    public String orderServiceFallBackMethodPost() {
         return "Order Service Not Working";
     }
 }
